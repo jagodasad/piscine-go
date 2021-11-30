09900fc03f74b34095947b4f0b6e788d220e0650
jagodasad <jago.sadowska@gmail.com>
2021-11-30 20:00:45 +0000

    My first commit

--- printcomb2.go
+++ printcomb2.go
@@ -3,18 +3,24 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for i := '00'; i <= '98'; i++ {
-		for j := i + 1; j <= '99'; j++ {
-			{
-				z01.PrintRune(i)
-				z01.PrintRune(' ')
-				z01.PrintRune(j)
-				
-				if i < '98' {
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
-				} else {
-					z01.PrintRune('\n')
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '8'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				for l := 1; l <= '9'; l++ {
+					{
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(l)
+
+						if i < '9' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						} else {
+							z01.PrintRune('\n')
+						}
+					}
 				}
 			}
 		}
