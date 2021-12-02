fc1503f9afa7e96f189fc75ef68901009656d0dc
jagodasad <jago.sadowska@gmail.com>
2021-12-02 17:37:49 +0000

    commit

--- basicatoi.go
+++ basicatoi.go
@@ -1,9 +1,14 @@
 package piscine
 
+/*import "strconv"
+
 func BasicAtoi(s string) int {
-	if string(s[0]) == "-" {
+	s := strconv.ParseInt(s)
+	if string(s[0]) == {
 		s = s[1:]
 	} else if string(s[0]) == "+" {
 		s = s[1:]
 	}
+	return int
 }
+*/
