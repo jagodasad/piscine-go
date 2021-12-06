45b1d12e1e9f06ec7c8e7d7487f094ab0fe58f41
jagodasad <jago.sadowska@gmail.com>
2021-12-06 16:34:14 +0000

    commit

--- test/test.go
+++ test/test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"piscine"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	fmt.Println(piscine.Sqrt(4))
-	fmt.Println(piscine.Sqrt(3))
+	z01.PrintRune(piscine.FirstRune("Hello!"))
+	z01.PrintRune(piscine.FirstRune("Salut!"))
+	z01.PrintRune(piscine.FirstRune("Ola!"))
+	z01.PrintRune('\n')
 }
