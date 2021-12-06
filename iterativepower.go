09cc3f45e80667e7044d42adf7be4a295fe77397
jagodasad <jago.sadowska@gmail.com>
2021-12-06 13:49:42 +0000

    commit

--- iterativepower.go
+++ iterativepower.go
@@ -2,16 +2,14 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 	result := 1
-	if nb <= 65 {
+	if nb <= 65 && power > 0 {
 		for i := 0; i < power; i++ {
 			result = result * nb
 		}
 	} else if power < 0 {
 		return 0
 	} else if power == 0 {
-		return 0
-	} else if power == 1 {
-		return nb
+		return 1
 	}
 	return result
 }
