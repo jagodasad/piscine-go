a545467fc61dfa340a9ce41b2f1bb64a29a9ee59
jagodasad <jago.sadowska@gmail.com>
2021-12-06 11:37:54 +0000

    commit

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,8 +2,12 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	for i := 1; i <= nb; i++ {
-		result = result * i
+	if result <= 25 {
+		for i := 1; i <= nb; i++ {
+			result = result * i
+		}
+	} else {
+		return 0
 	}
 	return result
 }
