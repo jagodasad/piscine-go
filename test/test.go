362d2757a64dbebaa92d0cbed1d7e690e456d005
jagodasad <jago.sadowska@gmail>
2021-12-07 13:32:15 +0000

    commit

--- test/test.go
+++ test/test.go
@@ -6,8 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.Index("Hello!", "l"))
-	fmt.Println(piscine.Index("Salut!", "alu"))
-	fmt.Println(piscine.Index("Ola!", "hOl"))
-	fmt.Println(piscine.Index("W0DeE#J]Cb+&t", "JA59K3 D_c'6;"))
+	fmt.Println(piscine.Concat("Hello!", " How are you?"))
 }
