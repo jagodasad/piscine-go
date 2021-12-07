081bf903fa0e2dc7e1df1a0e04adfd728c5ff83a
jagodasad <jago.sadowska@gmail>
2021-12-07 20:11:40 +0000

    commit

--- test/test.go
+++ test/test.go
@@ -1,11 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"piscine"
-)
+import "piscine"
 
 func main() {
-	fmt.Println(piscine.IsPrime(5))
-	fmt.Println(piscine.IsPrime(4))
+	piscine.PrintNbrInOrder(321)
+	piscine.PrintNbrInOrder(0)
+	piscine.PrintNbrInOrder(321)
 }
