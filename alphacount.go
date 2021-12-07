6439bf28c1e60b91cbec9eb539413347eab9d41e
jagodasad <jago.sadowska@gmail>
2021-12-07 11:33:38 +0000

    commit

--- alphacount.go
+++ alphacount.go
@@ -4,16 +4,8 @@ func AlphaCount(s string) int {
 	// sentence := []rune(s)
 	counter := 0
 	for _, c := range s {
-		n := c
-		for i := 'a'; i <= 'z'; i++ {
-			if n == i {
-				counter++
-			}
-			for i := 'A'; i <= 'Z'; i++ {
-				if n == i {
-					counter++
-				}
-			}
+		if (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') {
+			counter++
 		}
 	}
 	return counter
