3af9447b41771b617a4d13b8ad42bc48ec99a32c
jagodasad <jago.sadowska@gmail>
2021-12-07 12:36:06 +0000

    commit

--- index.go
+++ index.go
@@ -8,14 +8,17 @@ func Index(s string, toFind string) int {
 	if len(s) <= 0 || len(toFind) <= 0 {
 		return 0
 	}
+
 	for i := 0; i < len(s); i++ {
 		if sRune[i] == toFindRune[0] {
 			counter = counter + (i + 1)
 			break
 		}
 	}
+
 	if counter == 0 {
 		return -1
 	}
+
 	return counter - 1
 }
