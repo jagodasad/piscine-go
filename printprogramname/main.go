2545d9a98ec2d544fd4562b5893bb65c42d487b3
jagodasad <jago.sadowska@gmail.com>
2021-12-08 14:46:27 +0000

    commit

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"os"
+	"piscine"
 
 	"github.com/01-edu/z01"
 )
 
 func main() {
 	arguments := os.Args
-	abs := arguments[0][33:]
-	for _, x := range abs {
-		z01.PrintRune(x)
+	abs := arguments[0]
+
+	var result string
+
+	for i := len(abs) - 1; i >= 0; i-- {
+		if string(abs[i]) == "/" {
+			break
+		} else {
+			result += string(abs[i])
+		}
+	}
+	s := piscine.StrRev(result)
+	for _, l := range s {
+		z01.PrintRune(l)
 	}
 	z01.PrintRune('\n')
 }
