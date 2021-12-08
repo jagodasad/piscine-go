c59e686fe66d5deb10562aba83dbe1704b439574
jagodasad <jago.sadowska@gmail.com>
2021-12-08 14:55:40 +0000

    commit 3

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -6,28 +6,13 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func StrRev(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
-
 func main() {
 	arguments := os.Args
 	abs := arguments[0]
-	var result string
-	for i := len(abs) - 1; i >= 0; i-- {
-		if string(abs[i]) == "/" {
-			break
-		} else {
-			result += string(abs[i])
+	for i, l := range abs {
+		if i > 1 {
+			z01.PrintRune(rune(l))
 		}
-	}
-	s := StrRev(result)
-	for _, l := range s {
-		z01.PrintRune(l)
 	}
 	z01.PrintRune('\n')
 }
