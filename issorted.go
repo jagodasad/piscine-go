4bcfbd3d532b01e7cd9c5f4ac6c7670f1b0197eb
jagodasad <jago.sadowska@gmail.com>
2021-12-13 21:07:31 +0000

    commit

--- issorted.go
+++ issorted.go
@@ -3,14 +3,14 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	if len(a) > 1 {
 		if f(a[0], a[1]) >= 0 {
-			for i := 0; i < len(a) - 1; i++ {
+			for i := 0; i < len(a)-1; i++ {
 				if f(a[i], a[i+1]) < 0 {
 					return false
 				}
 			}
 		}
 		if f(a[0], a[1]) <= 0 {
-			for i := 0; i < len(a) - 1; i++ {
+			for i := 0; i < len(a)-1; i++ {
 				if f(a[i], a[i+1]) > 0 {
 					return false
 				}
@@ -18,6 +18,4 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 		}
 	}
 	return true
-
 }
-
