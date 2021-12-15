993d30ac417f88f0b8abeec6e5a800eae38e7d3e
jagodasad <jago.sadowska@gmail.com>
2021-12-15 12:09:49 +0000

    commit

--- test/test.go
+++ test/test.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"piscine"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	a1 := []int{0, 1, 2, 3, 4, 5}
-	a2 := []int{0, 2, 1, 3}
+	result := piscine.Rot14("Hello! How are You?")
 
-	result1 := piscine.IsSorted(f, a1)
-	result2 := piscine.IsSorted(f, a2)
-
-	fmt.Println(result1)
-	fmt.Println(result2)
+	for _, r := range result {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
 }
