75ad5ae647323b737e53a4f2065ff6ddabfe6f22
jagodasad <jago.sadowska@gmail.com>
2021-12-15 15:19:22 +0000

    commit

--- comcheck/main.go
+++ comcheck/main.go
@@ -5,18 +5,33 @@ import (
 	"os"
 )
 
-func main() {
-	r := ""
-	arg := os.Args[1:]
+// func main() {
+// 	r := ""
+// 	arg := os.Args[1:]
 
+// 	check := []string{"01", "galaxy", "galaxy 01"}
+// 	for _, res := range arg {
+// 		for _, i := range check {
+// 			if res == i {
+// 				r += "Alert!!!\n"
+// 				break
+// 			}
+// 		}
+// 	}
+// 	fmt.Print(r)
+// }
+func main() {
+	r := 0
+	arg := os.Args[1:]
 	check := []string{"01", "galaxy", "galaxy 01"}
-	for _, res := range arg {
-		for _, i := range check {
-			if res == i {
-				r += "Alert!!!\n"
-				break
+	for i := range arg {
+		for _, s := range check {
+			if arg[i] == s {
+				r++
 			}
 		}
 	}
-	fmt.Print(r)
+	if r >= 1 {
+		fmt.Println("Alert!!!")
+	}
 }
