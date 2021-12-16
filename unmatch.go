5b8bd221fd2d4ebc5321ec5883e8c226eb16efb7
jagodasad <jago.sadowska@gmail.com>
2021-12-16 08:21:47 +0000

    commit

--- unmatch.go
+++ unmatch.go
@@ -1,7 +1,7 @@
 package piscine
 
 func Unmatch(a []int) int {
-for i := 0; i < Lent4(a); i++ {
+	for i := 0; i < Lent4(a); i++ {
 		for j := i + 1; j < Lent4(a); j++ {
 			if a[i] == a[j] {
 				a[i] = -1
@@ -12,17 +12,16 @@ for i := 0; i < Lent4(a); i++ {
 	}
 	for k := 0; k < Lent4(a); k++ {
 		if a[k] != -1 {
-
 			return a[k]
 		}
 	}
-return -1
+	return -1
 }
 
-func Lent4(d []int) int{
-	inc:=0
-	for _, _= range d{
-		inc ++
+func Lent4(d []int) int {
+	inc := 0
+	for range d {
+		inc++
 	}
-	 return inc
+	return inc
 }
