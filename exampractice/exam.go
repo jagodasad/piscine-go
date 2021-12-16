fa5857138d53e4685639da02b5a1cb24beda8534
jagodasad <jago.sadowska@gmail.com>
2021-12-16 15:36:56 +0000

    commit

---
+++ exampractice/exam.go
@@ -0,0 +1,43 @@
+package main
+
+// package piscine
+
+// func rot14(b rune) rune {
+// 	if b >= 'A' && b < 'M' || b >= 'a' && b < 'm' {
+// 		return b + 14
+// 	}
+// 	if b >= 'M' && b <= 'Z' || b >= 'm' && b <= 'z' {
+// 		return b - 12
+// 	}
+// 	return b
+// }
+
+// func Rot14(s string) string {
+// 	result := ""
+// 	for _, res := range s {
+// 		result += string(rot14(res))
+// 	}
+// 	return results
+// }
+// args := os.Args
+// abs := args[0]
+// for i, l := range abs {
+// 	if i > l {
+// 		z01.PrintRune(rune(l))
+// 	}
+// 	z01.PrintRune('\n')
+// }
+
+// func main() {
+// 	arguments := os.Args
+// 	ln := 0
+// 	for i := range arguments {
+// 		ln = i
+// 	}
+// 	for j := 1; j <= ln; j++ {
+// 		for _, w := range arguments[j] {
+// 			z01.PrintRune(w)
+// 		}
+// 		z01.PrintRune('\n')
+// 	}
+// }
